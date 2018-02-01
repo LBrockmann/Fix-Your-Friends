840fc73e4e50bb630084cf117079eb4ca683e880
Luke Brockmann <lb3297@nyu.edu>
2018-02-01 07:38:30 -0800

    Created Scenes, Started Running Scene
    Player created
    Script of player movement just started
    UI created
    Nothing in other scenes

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    SpriteRenderer Player;
+    public float PlayerSpeed;
+    public Vector2 StartPos;
+    public Rigidbody2D rb;
+
+
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        this.GetComponent<Transform>().position = StartPos;
+        PlayerSpeed = 0.1f;
+    }
+
+
+    void Update()
+    {
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                PlayerSpeed = PlayerSpeed + 0.05f;
+                rb.AddForce(new Vector2(PlayerSpeed, 0), ForceMode2D.Impulse);
+            }
+            if (!Player.isVisible) //is ball sprite visible within the camera (SUPER USEFUL)
+            {
+                this.GetComponent<Transform>().position = StartPos; //reset position
+                PlayerSpeed = 0;
+                rb.velocity = new Vector2(0, 0);
+                rb.angularVelocity = 0f;
+            }
+        }
+    }
+}
