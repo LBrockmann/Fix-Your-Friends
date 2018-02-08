c50ff74b47c07eee93f2d5124c19c99a0c85a238
Luke Brockmann <lb3297@nyu.edu>
2018-02-08 08:59:51 -0800

    Bug Fixes and new timer

---
+++ Assets/Scripts/MenuScripts/CanvasManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasManager : MonoBehaviour {
+
+    public Text runButton; //Setting up text so the button can change what it is labeled as when it is pressed
+    public Text wagButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //Button Functions
+    public void OnRunButtonPressed ()
+    {
+        Debug.Log("ButtonPressed");
+        runButton.text = "please";
+        SceneManager.LoadScene("RunningScene"); //Whenever button is pushed, please should breifly be shown before loading the running game
+    }
+
+    public void OnWagButtonPressed ()
+    {
+        Debug.Log("ButtonPressed");
+        wagButton.text = "stop";
+        SceneManager.LoadScene("TellOffScene"); //Whenever button is pushed, stop should breifly be shown before loading the wagging game
+    }
+}
--- Assets/Scripts/RunningScript/WinCondition.cs
+++ Assets/Scripts/RunningScript/WinCondition.cs
@@ -5,17 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class WinCondition : MonoBehaviour
 {
+    private float timer;
 
     // Use this for initialization
     void Start()
     {
-
+        timer = 5; //resetting timer whenever mini game opens
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer = timer-1 * Time.deltaTime; //setting time limit for the game
+        Debug.Log(timer);
 
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene("MenuScene"); //If the timer runs out the player is pulled back into the menu without the bonus
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other) //Creating function for finish line in the running race
@@ -24,6 +31,7 @@ public class WinCondition : MonoBehaviour
         {
             Debug.Log("Collided"); 
             SceneManager.LoadScene("MenuScene"); //When the player crosses the finish line they should be pulled straight back to the menu
+            //Add onto the player stats somehow
         }
     }
 }
